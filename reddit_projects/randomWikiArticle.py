ba17e5177bf0f22243b3a037ca98db294c27641d
Creighton Ashton <CreightonAshton@gmail.com>
2014-07-15 21:48:25 -0700

    updates
    now it works

--- reddit_projects/randomWikiArticle.py
+++ reddit_projects/randomWikiArticle.py
@@ -1,18 +1,18 @@
 from bs4 import BeautifulSoup
-import urllib2
+from urllib.request import urlopen
 import webbrowser
 
-print ('enter=next article, y=open artcle, x=exit')
+print ('enter = next article, y = open artcle, x = exit')
 print ('-------------------------------------------')
 while True:
-    content=urllib2.urlopen('http://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=xml')
-    xml=content.read()
+    content = urlopen('http://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=xml')
+    xml = content.read()
     content.close()
-    soup=BeautifulSoup(xml)
-    links=soup.findAll('page')
+    soup = BeautifulSoup(xml)
+    links = soup.find_all('page')
     for l in links:
-        choice=input('Would you like to read about '+l.get('title').encode('ascii', 'ignore')+'?')
-        if choice=='yes':
+        choice = input('Would you like to read about ' + l.get('title') + '?')
+        if choice == 'y':
             webbrowser.open_new_tab('http://en.wikipedia.org/wiki?curid='+l.get('id'))
-        elif choice=='x':
+        elif choice == 'x':
             exit(0)
