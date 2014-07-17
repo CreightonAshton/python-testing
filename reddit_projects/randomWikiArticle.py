f0a7d51b429d22014d4631682b8b2a0832f87fd2
Creighton Ashton <CreightonAshton@gmail.com>
2014-07-17 15:22:41 -0700

    pulling from yahoo
    tried to pull information from yahoo, not working yet. unknown unknowns.

--- reddit_projects/randomWikiArticle.py
+++ reddit_projects/randomWikiArticle.py
@@ -5,14 +5,14 @@ import webbrowser
 print ('enter = next article, y = open artcle, x = exit')
 print ('-------------------------------------------')
 while True:
-    content = urlopen('http://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=xml')
-    xml = content.read()
-    content.close()
-    soup = BeautifulSoup(xml)
-    links = soup.find_all('page')
-    for l in links:
-        choice = input('Would you like to read about ' + l.get('title') + '?')
-        if choice == 'y':
-            webbrowser.open_new_tab('http://en.wikipedia.org/wiki?curid='+l.get('id'))
-        elif choice == 'x':
-            exit(0)
+	content = urlopen('http://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=xml')
+	xml = content.read()
+	content.close()
+	soup = BeautifulSoup(xml)
+	links = soup.find_all('page')
+	for l in links:
+		choice = input('Would you like to read about ' + l.get('title') + '?')
+		if choice == 'y':
+			webbrowser.open_new_tab('http://en.wikipedia.org/wiki?curid='+l.get('id'))
+		elif choice == 'x':
+			exit(0)
--- stockTracker.py
+++ stockTracker.py
@@ -1,10 +1,23 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
 num_stocks = int(input("How many stocks do you want to track? "))
 my_portfolio = {}
 
 for i in range(num_stocks):
 	my_stock_name = str(input("Name of stock: ")).upper()
-	my_stock_price = float(input("Current value of " + my_stock_name + ": "))
-	my_portfolio[my_stock_name] = my_stock_price
+	content = urlopen('http://finance.yahoo.com/q?s=' + my_stock_name)
+	page_holder = content.read()
+	content.close()
+	soup = BeautifulSoup(page_holder)
+	#print (soup.prettify())
+	span_id = 'yfs_184_' + my_stock_name.lower()
+	print (span_id)
+	print (soup.find_all('span'))
+	test = soup.find('yfs_184_')
+	print (test)
+	#my_stock_price = float(input("Current value of " + my_stock_name + ": "))
+	#my_portfolio[my_stock_name] = my_stock_price
 
 folio = open("portfolios\my_portfolio.txt", "r+")
 for stock, price in my_portfolio.items():
