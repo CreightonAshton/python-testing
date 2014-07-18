7f93c436f85da917b7ec768a31e588ef602da7ac
Creighton Ashton <CreightonAshton@gmail.com>
2014-07-18 13:15:52 -0700

    stock lookup
    now the program looks up the current value of the inputted stock and
    saves it to a portfolio that is datestamped

--- stockTracker.py
+++ stockTracker.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from datetime import datetime
 
 num_stocks = int(input("How many stocks do you want to track? "))
 my_portfolio = {}
@@ -10,17 +11,18 @@ for i in range(num_stocks):
 	page_holder = content.read()
 	content.close()
 	soup = BeautifulSoup(page_holder)
-	#print (soup.prettify())
-	span_id = 'yfs_184_' + my_stock_name.lower()
-	print (span_id)
-	print (soup.find_all('span'))
-	test = soup.find('yfs_184_')
-	print (test)
-	#my_stock_price = float(input("Current value of " + my_stock_name + ": "))
-	#my_portfolio[my_stock_name] = my_stock_price
+	span_id = 'yfs_l84_' + my_stock_name.lower()
+	span_list = soup.find_all('span')
+	my_stock_price = float(soup.find(id=span_id).string)
+	print ("Current value of ", my_stock_name, " is $", my_stock_price, ".", sep="")
+	my_portfolio[my_stock_name] = my_stock_price
 
-folio = open("portfolios\my_portfolio.txt", "r+")
+current_date = "{:%B %d, %Y}".format(datetime.now())
+folio = open("portfolios\my_portfolio.txt", "a")
+folio.write(str(current_date) + "\n")
 for stock, price in my_portfolio.items():
 	string_to_write = str(stock) + ": " + str(price) + "\n"
 	folio.write(string_to_write)
+folio.write("\n")
 folio.close()
+print("Portfolio has been saved")
